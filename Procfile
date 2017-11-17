037a669696a5f7505c8e7d34f56ac14721a19a38
etai83 <itaizloto@gmail.com>
2017-11-17 22:32:34 +0200

    update Procfile, remove ip and host

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn deploy:main
+web: gunicorn deploy:main --log-file
--- main.py
+++ main.py
@@ -80,5 +80,6 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='50.16.251.65', port=443, debug=True)
+    app.run(debug=True)
 # [END app]
+#host='50.16.251.65', port=443, 
